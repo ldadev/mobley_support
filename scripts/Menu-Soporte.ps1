4a79b2ff6eace224a481c9cc6ac3f0b32f92b02e
David <ldavidme7@gmail.com>
2026-08-28 10:08:45 -0500

    Centrar nombre del toolkit

--- scripts/Menu-Soporte.ps1
+++ scripts/Menu-Soporte.ps1
@@ -85,8 +85,8 @@ $opciones = [ordered]@{
 while ($true) {
     Clear-Host
     Write-Host ''
-    Write-MobleyHeader
-    Write-Titulo 'MOBLEY TOOLKIT - DIAGNOSTICO Y SOPORTE PC' -col Cyan
+    Write-Titulo 'MOBLEY TOOLKIT' -col Cyan
+    Write-Titulo 'DIAGNOSTICO Y SOPORTE PC' -col Cyan
     Write-Host ''
 
     $colNum = 'Cyan'
